e0365c4a884791155bca8543212cddd5b2900799
AartiPundalik <aartipundalik68@gmail.com>
2023-02-05 19:25:13 +0530

    [Aarti] Add : UC3-PartTime

--- EmployeeWage_problems/Program.cs
+++ EmployeeWage_problems/Program.cs
@@ -13,6 +13,12 @@ namespace EmployeeWage_problems
             Empcheck empcheck = new Empcheck();
             Empcheck.Attendance();
             Console.ReadLine();
+            UC2 emppcheck = new UC2();
+            UC2.empcheck();
+            Console.ReadLine();
+            UC3 empcheckk = new UC3();
+            UC3.PartTime();
+            Console.ReadLine();
 
         }
     }
---
+++ EmployeeWage_problems/UC3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage_problems
+{
+    internal class UC3
+    {
+        public static void empcheck()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+            int Is_Part_Time = 2;
+            int Empwage;
+            int Emphours;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Full Time");
+                Emphours = 8;
+            }
+            else if (empcheck == Is_Part_Time)
+                Emphours = 4;
+
+            else
+                Emphours = 0;
+
+            Empwage = Emphours * Emp_Rate_Per_Hour;
+
+            Console.WriteLine("Daily employee wage is : " + Empwage);
+
+
+        }
+    }
+}
+
